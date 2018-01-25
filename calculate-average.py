27d13cc57e1c431ea3ad518ccebd46feb8b70feb
Faridtsl <ayoujil.farid@gmail.com>
2018-01-25 15:27:29 +0000

    Final version

--- calculate-average.py
+++ calculate-average.py
@@ -1,28 +1,22 @@
 import subprocess
-from multiprocessing.dummy import Pool as ThreadPool
 
-
+"""
+	* Exacutes the program.
+	* Takes it's execution time.
+	* Return it.
+"""
 def gettime(a):
 	try:
-		a = subprocess.check_output(['casperjs','scrape-one-load.js'])
+		a = subprocess.check_output(['casperjs','scrape.js'])
 	except Exception, e:
 		a = str(e.output)
 	print a.split('\n')[0]
 	return float(a.split('\n')[0])
 
-
-def threading():
-	arr = [None]*100
-	pool = ThreadPool(10)
-	results = pool.map(gettime, arr)
-	sm = 0
-
-	for item in results:
-		sm += item
-
-	print sm/100
-
-
+"""
+	* Calls gettime 50 times.
+	* Prints the average of execution times
+"""
 def sequencial():
 	sm = 0
 	for i in xrange(50):
